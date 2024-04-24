3cffe4e053aeb78f8464e6390ca072261a5841ab
ADIGA612 <h97abaza4pro@gmail.com>
2024-04-24 11:26:14 +0300

    feat: add /api/fileanalyse endpoint

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
@@ -11,7 +14,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single("upfile"),(req,res,next)=>{
+  const upfile=req.file
+  const { originalname:name, mimetype:type, size:size } = upfile;
+  res.json({name,type,size})
+});
 
 
 const port = process.env.PORT || 3000;
